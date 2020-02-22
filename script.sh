8fd20efaab5835c6d7b856434d9d51d2aa89b3f9
vikash1sinha <vikash.sinha100@gmail.com>
2020-02-22 12:09:48 +0530

    editing script

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo mkdir -p /nginx
-sudo bash -c "echo 'Hello World!!!, I am nginx server serving static content, running using docker on ec2 instance managed by terraform and build using jenkins, deployed in CICD pipe
-line triggered by GIT push to master branch demostrated to Durgesh Jangid.' > index.html"
+cd /nginx
+sudo bash -c "echo 'Hello World, I am nginx server serving static content, running using docker on ec2 instance managed by terraform and build using jenkins, deployed in CICD pipe
+line triggered by GIT push to master branch.' > index.html"
 
 sudo docker run -d -p 80:80 -v /nginx/:/usr/share/nginx/html/ --name nginx nginx
