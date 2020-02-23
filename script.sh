0f09309b2594a4daa7fdad5eeeb40351ea205313
vikash1sinha <vikash.sinha100@gmail.com>
2020-02-23 12:17:08 +0530

    Running node image

--- script.sh
+++ script.sh
@@ -9,3 +9,4 @@ sudo bash -c "echo 'Hello World, I am nginx server serving static content, runni
 line triggered by GIT push to master branch.' > index.html"
 
 sudo docker run -d -p 80:80 -v /nginx/:/usr/share/nginx/html/ --name nginx nginx
+sudo docker run -d shubh228/simple-nodejs:v1 -p 8080:8080
