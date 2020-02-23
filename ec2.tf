9a3de9e1c8d8bc6e1cb22052766859568f1a8b74
vikash1sinha <vikash.sinha100@gmail.com>
2020-02-23 21:45:29 +0530

    editing script

--- ec2.tf
+++ ec2.tf
@@ -57,9 +57,16 @@ resource "aws_security_group" "dockersg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   
    ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
