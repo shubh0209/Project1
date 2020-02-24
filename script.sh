638f8acee543981c3fbbb4304fbbcc8b188aed8c
vikash1sinha <vikash.sinha100@gmail.com>
2020-02-24 21:05:37 +0530

    editing script

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo mkdir -p /nginx
 cd /nginx
-sudo bash -c "echo 'Hello World, I am  nginx server which serves static content, running using docker on ec2 instance managed by terraform and build using jenkins, deployed in CICD pipe
-line triggered by a GIT push to master branch.' > index.html"
+sudo bash -c "echo 'Hello World, I am nginx server which serves static content, running using docker on  amazon ec2 instance managed by terraform and build using jenkins, deployed in CICD pipe
+line, triggered by a GIT push to master branch.' > index.html"
 
 sudo docker run -d -p 80:80 -v /nginx/:/usr/share/nginx/html/ --name nginx nginx
 sudo docker run -d -p 8080:8080 shubh228/simple-nodejs:v1
